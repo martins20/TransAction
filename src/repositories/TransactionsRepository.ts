3be38d2ba506d35a5dff3e72e189aba6beb75f07
martins20 <pdesignerpb@gmail.com>
2020-08-25 06:55:35 -0300

    Challenger completed git add .

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -13,16 +19,45 @@ class TransactionsRepository {
     this.transactions = [];
   }
 
-  public all(): Transaction[] {
-    // TODO
+  all(): Transaction[] {
+    return this.transactions;
   }
 
-  public getBalance(): Balance {
-    // TODO
+  getBalance(): Balance {
+    const { income, outcome } = this.transactions.reduce(
+      (accumulator: Balance, transaction: Transaction) => {
+        switch (transaction.type) {
+          case 'income':
+            accumulator.income += transaction.value;
+            break;
+          case 'outcome':
+            accumulator.outcome += transaction.value;
+            break;
+
+          default:
+            break;
+        }
+
+        return accumulator;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
+
+    const total = income - outcome;
+
+    return { income, outcome, total };
   }
 
-  public create(): Transaction {
-    // TODO
+  create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transation = new Transaction({ title, value, type });
+
+    this.transactions.push(transation);
+
+    return transation;
   }
 }
 
